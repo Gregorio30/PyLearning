957f36a93bc5948884d68dcebcbc6b29fe00885b
Gregorio30 <joaovicenteag.tj@gmail.com>
2025-08-13 21:49:58 -0300

    Arquivo da Aula 4

---
+++ Aula4.js
@@ -0,0 +1,15 @@
+let numeroa = 10
+let numerob = 20
+let numeroc = 30
+
+let soma = numeroa + numerob + numeroc
+console.log ("a soma dos números é " + soma)
+
+let subtracao = numeroa - numerob - numeroc
+console.log ("a subtração dos números é " + subtracao)
+
+let multiplicacao = numeroa * numerob
+console.log ("a multiplicação dos números é " + multiplicacao)
+
+let divisao = numeroa / numerob
+console.log ("a divisão dos números é " + divisao)
