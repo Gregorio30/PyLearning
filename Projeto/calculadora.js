2f4f42f7289389e85205edef6745cb58a4941e9f
Gregorio30 <joaovicenteag.tj@gmail.com>
2025-08-15 19:52:59 -0300

    Melhorias de boas praticas

--- Projeto/calculadora.js
+++ Projeto/calculadora.js
@@ -3,8 +3,18 @@ let num2 = 20
 let operador = "Multiplicação"
 
 switch (operador)
-    {case "Multiplicação":console.log(num1*num2);break
-    case "Divisão":console.log(num1/num2);break
-    case "Soma":console.log(num1+num2);break
-    case "Subtração":console.log(num1-num2);break
-    default: console.log("Operação Invalida")}
+    {case "Multiplicação":
+        console.log(num1*num2);
+        break
+    case "Divisão":
+        console.log(num1/num2);
+        break
+    case "Soma":
+        console.log(num1+num2);
+        break
+    case "Subtração":
+        console.log(num1-num2);
+        break
+    default: 
+    console.log("Operação Invalida")
+}
