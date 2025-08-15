7ed108471ea4b6365b74265f944e5ee764a443d7
Gregorio30 <joaovicenteag.tj@gmail.com>
2025-08-15 17:53:36 -0300

    Atividade postada via terminal bash

---
+++ Projeto/calculadora.js
@@ -0,0 +1,9 @@
+let num1 = 10
+let num2 = 20
+let operador = "Multiplicação"
+
+switch (operador)
+    {case "Multiplicação":console.log(num1*num2);break
+    case "Divisão":console.log(num1/num2);break
+    case "Soma":console.log(num1+num2);break
+    case "Subtração":console.log(num1-num2);break}
